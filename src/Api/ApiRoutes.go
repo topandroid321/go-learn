36c7b59d447c7656096bbb50788969e9a55c5193
ibnurizqiaramadan <dadanibnu61@gmail.com>
2023-09-01 14:27:30 +0700

    move logger and auth middleware to client.go

--- src/Api/ApiRoutes.go
+++ src/Api/ApiRoutes.go
@@ -5,18 +5,11 @@ import (
 	GetQuery "go-learning/src/Api/handlers/HowToGetQuery"
 	"go-learning/src/Api/handlers/Index"
 	"go-learning/src/Api/handlers/Users"
-	Middleware "go-learning/src/Api/middleware"
-
-	// "go-learning/src/Client"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func InitRoutes(http *fiber.App) {
-	http.Use(func(c *fiber.Ctx) error { return Middleware.CheckAuth(c) }) // auth middleware check
-	http.Use(logger.New())                                                // logging
-
 	http.Get("/", func(c *fiber.Ctx) error { return Index.Index(c) })
 	http.Get("/users", func(c *fiber.Ctx) error { return Users.AddUsers(c) })
 	http.Get("/get-admin", func(c *fiber.Ctx) error { return GetQuery.ExampleGetUsingAdmin(c) })
--- src/Client/Client.go
+++ src/Client/Client.go
@@ -4,32 +4,36 @@ import (
 	"fmt"
 
 	ApiRoutes "go-learning/src/Api"
+	Middleware "go-learning/src/Api/middleware"
 	"go-learning/src/Interfaces"
 
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
-var App = fiber.New(fiber.Config{
-	Prefork:       true,
-	CaseSensitive: true,
-	StrictRouting: true,
-	ServerHeader:  "Fiber",
-	AppName:       "Test App v1.0.1",
-	JSONEncoder:   json.Marshal,
-	JSONDecoder:   json.Unmarshal,
-})
-
 func RunServer(params Interfaces.SystemInterface) {
-	App.Use(cors.New(cors.Config{
+	app := fiber.New(fiber.Config{
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Fiber",
+		AppName:       "Test App v1.0.1",
+		JSONEncoder:   json.Marshal,
+		JSONDecoder:   json.Unmarshal,
+	})
+	app.Use(cors.New(cors.Config{
 		AllowOrigins: params.CorsAllowOrigin,
 		AllowHeaders: params.CorsAllowHeader,
 	}))
 
-	ApiRoutes.InitRoutes(App)
-	App.Get("/metrics", monitor.New())
+	app.Use(func(c *fiber.Ctx) error { return Middleware.CheckAuth(c) }) // auth middleware check
+	app.Use(logger.New())                                                // logging
 
-	App.Listen("localhost:" + fmt.Sprint(params.Port))
+	ApiRoutes.InitRoutes(app)
+	app.Get("/metrics", monitor.New())
+
+	app.Listen("localhost:" + fmt.Sprint(params.Port))
 }
